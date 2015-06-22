51c52fb02660b3f7df86d1ccca1edec8d148c1ad
athiramukundan <athira_tech@yahoo.in>
2015-06-23 00:25:30 +0530

    cleaned up code. added meaniful logs

--- LinkCrawler.js
+++ LinkCrawler.js
@@ -2,7 +2,8 @@
 var depth = depthRemaining = 150;
 var urlsCrawled = 0;
 var result = new Array();
-var urlToCrawl = "https://python.org";
+var urlToCrawl = "https://www.facebook.com";
+
 var path = require('path');
 var Crawler = require(path.resolve( __dirname, "./crawler.js"));
 
@@ -12,50 +13,58 @@ var regexForLink =/(\b(https?|www|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z
 
 Crawler.prototype.crawlForLinks = function crawlForLinks(){
   var self = this;
-  urlsCrawled += 1; 
+  urlsCrawled += 1;
+
+  //Success CallBack
   var onSuccess = function(matchedItems ){
     HandleItemsReturned(matchedItems);
     if(depthRemaining != 0){
-      if(urlsCrawled === result.length){
-      	onComplete();
+      if(((urlsCrawled - 1) == result.length) && (result.length != 0)){
+        //All URLs are exhauted. Cannot crawl anymore
         console.log("Exhauted all links");
       }
       else{
-      	urlToCrawl = result[urlsCrawled-1];
-      	crawlForLinks();
+        urlToCrawl = result[urlsCrawled-1];
+        crawlForLinks();
       }
     }
   };
+
+  //Error CallBack
   var onFailure = function(){
     console.log("ERROR: Could not crawl to URL-"+ urlToCrawl);
     if((urlsCrawled - 1) != result.length){
-    	urlToCrawl = result[urlsCrawled-1];
-      	crawlForLinks();
+      urlToCrawl = result[urlsCrawled-1];
+        crawlForLinks();
     }
   };
   
+  //Required depth is met. Return to caller.
   if(result.length >= depth || ((urlsCrawled -1) == result.length) && result.length != 0)
     return;
+  
+  //Recursive crawl till depth satisfied.
   console.log("\n\nCrawling to URL:" + urlToCrawl + "\n")
   crawler_obj.init(urlToCrawl);
   crawler_obj.crawl(regexForLink,onSuccess,onFailure);
 }
 
 
-function HandleItemsReturned(itemsReturned){
-	//console.log("depthRemaining"+depthRemaining);
-
-  if(itemsReturned.length > depthRemaining){
-  	result = result.concat(itemsReturned.splice(0,depthRemaining));
-  	console.log(itemsReturned.splice(0,depthRemaining));
-  }
-  else{
-  	result = result.concat(itemsReturned);
-  	console.log(itemsReturned);
-  }
+function HandleItemsReturned(itemsReturned)
+{
+  var itemToPrint;
+  if(itemsReturned.length > depthRemaining)
+    itemToPrint = itemsReturned.splice(0,depthRemaining);
+  else
+    itemToPrint = itemsReturned
+  result = result.concat(itemToPrint);
   depthRemaining = depth - result.length;
+
+  //Print Data
+  console.log(itemToPrint);
   console.log("Total links added to the list:"+result.length);
 }
 
+//Initiate Crawling.
 var crawler_obj = new Crawler();
 crawler_obj.crawlForLinks();
